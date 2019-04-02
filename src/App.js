3430801bf559687dd1c4008eb2d94950d88b469b
unknown <mattgvo@gmail.com>
2019-04-02 11:49:23 -0600

    ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './AtNite.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+         <h1 className="neon">DevMtn @ Nite</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>With Your Host: </p>
+          <h3>Matt Van Orman A.K.A "Nintendo Jones"!</h3>
+          {/* <h3>Jonathan "Old" McDonald!</h3> */}
+          {/* <h3>David Telford, The Thrash Master!</h3> */}
         </header>
       </div>
     );
